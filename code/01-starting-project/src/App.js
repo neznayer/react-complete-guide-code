a367b7fc24ed2169c04e6c38bf81b77824cdfbf9
Anton <nokker@gmail.com>
2022-12-18 18:26:29 +0900

    show: add localStorage set

--- code/01-starting-project/src/App.js
+++ code/01-starting-project/src/App.js
@@ -10,10 +10,12 @@ function App() {
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.setItem('isLoggedIn', '0');
     setIsLoggedIn(false);
   };
 
