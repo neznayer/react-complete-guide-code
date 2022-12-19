77c742a31f0b79627954d7d70f698ff674b2d299
Anton <nokker@gmail.com>
2022-12-19 13:06:13 +0900

    show: fix reducers

--- code/06-usereducer-starting-project/src/components/Login/Login.js
+++ code/06-usereducer-starting-project/src/components/Login/Login.js
@@ -87,13 +87,14 @@ const Login = (props) => {
 
   const emailChangeHandler = (event) => {
     dispatchEmail({ type: 'USER_INPUT', payload: event.target.value });
-    setFormIsValid(emailState.isValid && passwordState.isValid);
+
+    setFormIsValid(event.target.value.includes('@') && passwordState.isValid);
   };
 
   const passwordChangeHandler = (event) => {
     dispatchPassword({ type: 'USER_INPUT', payload: event.target.value });
 
-    setFormIsValid(emailState.isValid && passwordState.isValid);
+    setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
   };
 
   const validateEmailHandler = () => {
